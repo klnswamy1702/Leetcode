ffbfcee6df0944a8f616e45cc90ef0eac5ee01b4
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2023-11-07 20:46:46 +0530

    Time: 49 ms (98.79%) | Memory: 17.8 MB (15.66%) - LeetSync

---
+++ 526-beautiful-arrangement/beautiful-arrangement.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countArrangement(self, N: int) -> int:
+        @lru_cache(None)
+        def dfs(bm, pl):
+            if pl == 0: return 1
+                
+            S = 0
+            for i in range(N):
+                if not bm&1<<i and ((i+1)%pl == 0 or pl%(i+1) == 0):
+                    S += dfs(bm^1<<i, pl - 1)
+            return S
+                
+        return dfs(0, N)
+        
