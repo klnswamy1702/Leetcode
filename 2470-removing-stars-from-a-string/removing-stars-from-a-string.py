487c717c363f3f2367f1dfd49e039558df9656bb
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2023-11-12 00:18:28 +0530

    Time: 199 ms (47.57%) | Memory: 17.8 MB (72.33%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        res = []
+        for c in s :
+            if res and c == '*':
+                res.pop()
+            else:
+                res.append(c)
+        return ''.join(res)
