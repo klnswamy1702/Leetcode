4708c93ddd093475c6fa1a89c6969bb6eedbffd9
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2023-11-18 00:01:27 +0530

    Time: 377 ms (23.72%) | Memory: 18.2 MB (77.85%) - LeetSync

---
+++ 2001-jump-game-vii/jump-game-vii.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canReach(self, s: str, minJ: int, maxJ: int) -> bool:
+        dp = [c == '0' for c in s]
+        pre = 0
+        for i in range(1, len(s)):
+            if i >= minJ: pre += dp[i - minJ]
+            if i > maxJ: pre -= dp[i - maxJ - 1]
+            dp[i] &= pre > 0
+        return dp[-1]
+        
