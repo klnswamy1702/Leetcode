6d0a56c76fd307fdc3c2f3f417a07ad2a8a14e5c
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2023-12-02 20:36:35 +0530

    Time: 535 ms (36.80%) | Memory: 23.8 MB (51.52%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,17 +1,5 @@
-# class Solution:
-#     def maxArea(self, height: List[int]) -> int:
-#         i, j = 0, len(height) - 1
-#         water = 0
-#         while i < j:
-#             water = max(water, (j - i) * min(height[i], height[j]))
-#             if height[i] < height[j]:
-#                 i += 1
-#             else:
-#                 j -= 1
-#         return water 
-
-class Solution:
-    def maxArea(self, height: List[int]) -> int:
+class Solution(object):
+    def maxArea(self, height):
         l, r = 0, len(height) - 1
         res = 0
 
