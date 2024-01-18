3628d0f5b8331157080e88b18363f9792cd14e25
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2024-01-18 21:52:51 +0530

    Time: 36 ms (69.06%) | Memory: 17.5 MB (9.64%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,20 +1,26 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        dic = { "2": "abc", "3": "def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
-        
-        res=[]
-        if len(digits) ==0:
-            return res
-            
-        self.dfs(digits, 0, dic, '', res)
-        return res
-    
-    def dfs(self, nums, index, dic, path, res):
-        if index >=len(nums):
-            res.append(path)
-            return
-        string1 =dic[nums[index]]
-        for i in string1:
-            self.dfs(nums, index+1, dic, path + i, res)
+        res = []
+        digitToChar = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "qprs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
 
+        def backtrack(i, curStr):
+            if len(curStr) == len(digits):
+                res.append(curStr)
+                return
+            for c in digitToChar[digits[i]]:
+                backtrack(i + 1, curStr + c)
+
+        if digits:
+            backtrack(0, "")
+
+        return res
         
