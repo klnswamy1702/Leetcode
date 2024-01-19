fb0b27e385dd158e646d9befa5307cf0db309476
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2024-01-19 17:15:56 +0530

    Time: 202 ms (89.02%) | Memory: 12 MB (100.00%) - LeetSync

--- 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.py
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.py
@@ -1,5 +1,16 @@
-class Solution:
-    def addToArrayForm(self, A: List[int], K: int) -> List[int]:
-        for i in range(len(A) - 1, -1, -1):
-            K, A[i] = divmod(A[i] + K, 10)
-        return [int(i) for i in str(K)] + A if K else A
+class Solution(object):
+    def addToArrayForm(self, A, K):
+        A[-1] += K
+        for i in xrange(len(A) - 1, -1, -1):
+            carry, A[i] = divmod(A[i], 10)
+            if i: A[i-1] += carry
+        if carry:
+            A = map(int, str(carry)) + A
+        return A
+
+        """
+        :type num: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        
