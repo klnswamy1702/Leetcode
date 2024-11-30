cff650cc34fffb23818a6f12d8d08832bf04f8ea
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2024-11-30 12:53:28 +0530

    Time: 600 ms (7.50%) | Memory: 0B (100.00%) - LeetSync

--- 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -1,6 +1,14 @@
-SELECT e1.employee_id
-FROM Employees e1
-LEFT JOIN Employees e2
-ON e1.manager_id = e2.employee_id
-WHERE e1.salary < 30000 AND e2.employee_id IS NULL AND e1.manager_id IS NOT NULL
+-- SELECT e1.employee_id
+-- FROM Employees e1
+-- LEFT JOIN Employees e2
+-- ON e1.manager_id = e2.employee_id
+-- WHERE e1.salary < 30000 AND e2.employee_id IS NULL AND e1.manager_id IS NOT NULL
+-- ORDER BY employee_id;
+
+SELECT employee_id
+FROM Employees
+WHERE salary < 30000
+AND manager_id NOT IN (
+  SELECT employee_id FROM Employees
+)
 ORDER BY employee_id;
