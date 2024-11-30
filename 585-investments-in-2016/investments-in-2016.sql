320249480910950c20d2834d52164f7e61e8fa49
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2024-11-30 12:58:02 +0530

    Time: 485 ms (94.85%) | Memory: 0B (100.00%) - LeetSync

--- 585-investments-in-2016/investments-in-2016.sql
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -1,9 +1,30 @@
 SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
-FROM (
-   SELECT *,
-       COUNT(*)OVER(PARTITION BY tiv_2015) AS tiv_2015_cnt,
-       COUNT(*)OVER(PARTITION BY lat, lon) AS loc_cnt
+FROM Insurance i
+JOIN
+   (
+   SELECT tiv_2015
    FROM Insurance
+   GROUP BY tiv_2015
+   HAVING COUNT(DISTINCT pid) > 1
    )t0
-WHERE tiv_2015_cnt > 1
-AND loc_cnt = 1
+ON i.tiv_2015 = t0.tiv_2015
+JOIN
+   (
+   SELECT CONCAT(lat, lon) lat_lon
+   FROM Insurance
+   GROUP BY CONCAT(lat, lon)
+   HAVING COUNT(DISTINCT pid) = 1
+   )t1
+ON CONCAT(i.lat, i.lon) = t1.lat_lon
+
+
+-- -- Approach 2
+-- SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
+-- FROM (
+--    SELECT *,
+--        COUNT(*)OVER(PARTITION BY tiv_2015) AS tiv_2015_cnt,
+--        COUNT(*)OVER(PARTITION BY lat, lon) AS loc_cnt
+--    FROM Insurance
+--    )t0
+-- WHERE tiv_2015_cnt > 1
+-- AND loc_cnt = 1
