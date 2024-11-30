c8b35c247c84356e25e23d53c280dc13bea5794e
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2024-11-30 12:42:53 +0530

    Time: 640 ms (39.23%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,19 +1,35 @@
--- Retrieving employees with primary_flag set to 'Y'
+-- -- Retrieving employees with primary_flag set to 'Y'
+-- SELECT 
+--   employee_id, 
+--   department_id 
+-- FROM 
+--   Employee 
+-- WHERE 
+--   primary_flag = 'Y' 
+-- UNION 
+-- -- Retrieving employees that appear exactly once in the Employee table
+-- SELECT 
+--   employee_id, 
+--   department_id 
+-- FROM 
+--   Employee 
+-- GROUP BY 
+--   employee_id 
+-- HAVING 
+--   COUNT(employee_id) = 1;
+
+
 SELECT 
   employee_id, 
   department_id 
 FROM 
-  Employee 
+  (
+    SELECT 
+      *, 
+      COUNT(employee_id) OVER(PARTITION BY employee_id) AS EmployeeCount
+    FROM 
+      Employee
+  ) EmployeePartition 
 WHERE 
-  primary_flag = 'Y' 
-UNION 
--- Retrieving employees that appear exactly once in the Employee table
-SELECT 
-  employee_id, 
-  department_id 
-FROM 
-  Employee 
-GROUP BY 
-  employee_id 
-HAVING 
-  COUNT(employee_id) = 1;
+  EmployeeCount = 1 
+  OR primary_flag = 'Y';
