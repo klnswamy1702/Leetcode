4767288971fc34fd29bcae7b9359c5bfd1486bb3
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2024-11-30 12:40:50 +0530

    Time: 812 ms (27.58%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -1,14 +1,38 @@
+-- SELECT 
+--   mgr.employee_id, 
+--   mgr.name, 
+--   COUNT(emp.employee_id) AS reports_count, 
+--   ROUND(
+--     AVG(emp.age)
+--   ) AS average_age 
+-- FROM 
+--   employees emp 
+--   JOIN employees mgr ON emp.reports_to = mgr.employee_id 
+-- GROUP BY 
+--   employee_id 
+-- ORDER BY 
+--   employee_id
+
+
 SELECT 
-  mgr.employee_id, 
-  mgr.name, 
-  COUNT(emp.employee_id) AS reports_count, 
+  reports_to AS employee_id, 
+  (
+    SELECT 
+      name 
+    FROM 
+      employees e1 
+    WHERE 
+      e.reports_to = e1.employee_id 
+  ) AS name, 
+  COUNT(reports_to) AS reports_count, 
   ROUND(
-    AVG(emp.age)
+    AVG(age)
   ) AS average_age 
 FROM 
-  employees emp 
-  JOIN employees mgr ON emp.reports_to = mgr.employee_id 
+  employees e 
 GROUP BY 
-  employee_id 
+  reports_to 
+HAVING 
+  reports_count > 0 
 ORDER BY 
   employee_id
