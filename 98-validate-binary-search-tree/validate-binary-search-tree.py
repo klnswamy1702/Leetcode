80ab3bd1c08ff1c3d22dc720d257d1114f690cc6
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-01-01 12:14:06 +0530

    Time: 1 ms (48.83%) | Memory: 19.8 MB (6.77%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -6,15 +6,19 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def valid(node, left, right):
+        def validate(node, low=-math.inf, high=math.inf):
+            # Empty trees are valid BSTs.
             if not node:
                 return True
-            if not (left < node.val < right):
+
+            # The current node's value must be between low and high.
+            if node.val <= low or node.val >= high:
                 return False
 
-            return valid(node.left, left, node.val) and valid(
-                node.right, node.val, right
+            # The left and right subtree must also be valid.
+            return validate(node.right, node.val, high) and validate(
+                node.left, low, node.val
             )
 
-        return valid(root, float("-inf"), float("inf"))
+        return validate(root)
         
