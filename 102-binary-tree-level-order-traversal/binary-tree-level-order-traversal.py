b5c86b275ee73f76a3085ee3138afa4c19669a62
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-01-01 12:39:26 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (13.42%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -6,22 +6,35 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        res = []
-        q = collections.deque()
-        if root:
-            q.append(root)
-
-        while q:
-            val = []
-
-            for i in range(len(q)):
-                node = q.popleft()
-                val.append(node.val)
+        # levels = []
+        # if not root:
+        #     return levels
+        # level = 0
+        # queue = deque([root,])
+        # while queue:
+        #     levels.append([])
+        #     level_length = len(queue)
+        #     for i in range(level_length):
+        #         node = queue.popleft()
+        #         levels[level].append(node.val)
+        #         if node.left:
+        #             queue.append(node.left)
+        #         if node.right:
+        #             queue.append(node.right)
+        #     level += 1
+        # return levels
+        if not root: return []
+        queue, res = deque([root]), []
+        
+        while queue:
+            cur_level, size = [], len(queue)
+            for i in range(size):
+                node = queue.popleft()
                 if node.left:
-                    q.append(node.left)
+                    queue.append(node.left)
                 if node.right:
-                    q.append(node.right)
-            res.append(val)
+                    queue.append(node.right)
+                cur_level.append(node.val)
+            res.append(cur_level)
         return res
-
         
