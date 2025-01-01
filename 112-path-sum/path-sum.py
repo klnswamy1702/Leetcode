3484fe8521a65ec8fbd2db9b4fef7d8966c2f823
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-01-01 14:57:26 +0530

    Time: 0 ms (100.00%) | Memory: 18.8 MB (13.60%) - LeetSync

--- 112-path-sum/path-sum.py
+++ 112-path-sum/path-sum.py
@@ -6,28 +6,28 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        # if not root:
-        #     return False
-
-        # sum -= root.val
-        # if not root.left and not root.right:  # if reach a leaf
-        #     return sum == 0
-        # return self.hasPathSum(root.left, sum) or self.hasPathSum(
-        #     root.right, sum
-        # )
         if not root:
             return False
 
-        de = [
-            (root, targetSum - root.val),
-        ]
-        while de:
-            node, curr_sum = de.pop()
-            if not node.left and not node.right and curr_sum == 0:
-                return True
-            if node.right:
-                de.append((node.right, curr_sum - node.right.val))
-            if node.left:
-                de.append((node.left, curr_sum - node.left.val))
-        return False
+        targetSum -= root.val
+        if not root.left and not root.right:  # if reach a leaf
+            return targetSum == 0
+        return self.hasPathSum(root.left, targetSum) or self.hasPathSum(
+            root.right, targetSum
+        )
+        # if not root:
+        #     return False
+
+        # de = [
+        #     (root, targetSum - root.val),
+        # ]
+        # while de:
+        #     node, curr_sum = de.pop()
+        #     if not node.left and not node.right and curr_sum == 0:
+        #         return True
+        #     if node.right:
+        #         de.append((node.right, curr_sum - node.right.val))
+        #     if node.left:
+        #         de.append((node.left, curr_sum - node.left.val))
+        # return False
         
