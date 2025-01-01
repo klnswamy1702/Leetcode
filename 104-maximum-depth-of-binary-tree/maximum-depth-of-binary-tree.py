40e2bc2ab040a2a8335560ef8a163a45d2741a6c
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-01-01 12:42:43 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (16.19%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -12,16 +12,29 @@ class Solution:
         #     left_height = self.maxDepth(root.left)
         #     right_height = self.maxDepth(root.right)
         #     return max(left_height, right_height) + 1
-        stack = []
-        if root is not None:
-            stack.append((1, root))
+        # stack = []
+        # if root is not None:
+        #     stack.append((1, root))
 
+        # depth = 0
+        # while stack != []:
+        #     current_depth, root = stack.pop()
+        #     if root is not None:
+        #         depth = max(depth, current_depth)
+        #         stack.append((current_depth + 1, root.left))
+        #         stack.append((current_depth + 1, root.right))
+
+        # return depth
         depth = 0
-        while stack != []:
-            current_depth, root = stack.pop()
-            if root is not None:
-                depth = max(depth, current_depth)
-                stack.append((current_depth + 1, root.left))
-                stack.append((current_depth + 1, root.right))
-
+        level = [root] if root else []
+        while level:
+            depth += 1
+            queue = []
+            for el in level:
+                if el.left:
+                    queue.append(el.left)
+                if el.right:
+                    queue.append(el.right)
+            level = queue
+            
         return depth
