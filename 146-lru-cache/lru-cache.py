77d789985beb001321ded45cc201c6c1ef00f94b
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-01-01 15:10:59 +0530

    Time: 142 ms (55.11%) | Memory: 78.7 MB (11.12%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,25 +1,82 @@
-import collections
+# import collections
+
+
+# class LRUCache:
+#     def __init__(self, capacity: int):
+#         self.capacity = capacity
+#         self.dic = collections.OrderedDict()
+
+#     def get(self, key: int) -> int:
+#         if key not in self.dic:
+#             return -1
+
+#         self.dic.move_to_end(key)
+#         return self.dic[key]
+
+#     def put(self, key: int, value: int) -> None:
+#         if key in self.dic:
+#             self.dic.move_to_end(key)
+
+#         self.dic[key] = value
+#         if len(self.dic) > self.capacity:
+#             self.dic.popitem(False)
+
+
+# # Your LRUCache object will be instantiated and called as such:
+# # obj = LRUCache(capacity)
+# # param_1 = obj.get(key)
+# # obj.put(key,value)
+
+class ListNode:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.next = None
+        self.prev = None
 
 
 class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.dic = collections.OrderedDict()
+        self.dic = {}
+        self.head = ListNode(-1, -1)
+        self.tail = ListNode(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
     def get(self, key: int) -> int:
         if key not in self.dic:
             return -1
 
-        self.dic.move_to_end(key)
-        return self.dic[key]
+        node = self.dic[key]
+        self.remove(node)
+        self.add(node)
+        return node.val
 
     def put(self, key: int, value: int) -> None:
         if key in self.dic:
-            self.dic.move_to_end(key)
+            old_node = self.dic[key]
+            self.remove(old_node)
 
-        self.dic[key] = value
+        node = ListNode(key, value)
+        self.dic[key] = node
+        self.add(node)
+
         if len(self.dic) > self.capacity:
-            self.dic.popitem(False)
+            node_to_delete = self.head.next
+            self.remove(node_to_delete)
+            del self.dic[node_to_delete.key]
+
+    def add(self, node):
+        previous_end = self.tail.prev
+        previous_end.next = node
+        node.prev = previous_end
+        node.next = self.tail
+        self.tail.prev = node
+
+    def remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
 
 
 # Your LRUCache object will be instantiated and called as such:
