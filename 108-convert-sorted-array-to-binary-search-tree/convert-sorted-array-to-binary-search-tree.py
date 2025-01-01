7379a49770259e6b0f3811cbcf71c21234e8b746
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-01-01 12:57:13 +0530

    Time: 4 ms (27.00%) | Memory: 18.9 MB (21.29%) - LeetSync

--- 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.py
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.py
@@ -6,24 +6,27 @@
 #         self.right = right
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
-        # if not nums:
-        #     return None
-        # i = len(nums) // 2
-        # root = TreeNode(nums[i])
-        # root.left = self.sortedArrayToBST(nums[:i])
-        # root.right = self.sortedArrayToBST(nums[i+1:])
-        # return root
-        def helper(left, right):
-            if left > right:
-                return None
+        if not nums:
+            return None
 
-            # always choose left middle node as a root
-            p = (left + right) // 2
+        mid = len(nums) // 2
 
-            # preorder traversal: node -> left -> right
-            root = TreeNode(nums[p])
-            root.left = helper(left, p - 1)
-            root.right = helper(p + 1, right)
-            return root
+        root = TreeNode(nums[mid])
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid+1:])
 
-        return helper(0, len(nums) - 1)
+        return root
+        # def helper(left, right):
+        #     if left > right:
+        #         return None
+
+        #     # always choose left middle node as a root
+        #     p = (left + right) // 2
+
+        #     # preorder traversal: node -> left -> right
+        #     root = TreeNode(nums[p])
+        #     root.left = helper(left, p - 1)
+        #     root.right = helper(p + 1, right)
+        #     return root
+
+        # return helper(0, len(nums) - 1)
