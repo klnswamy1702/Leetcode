e32ec2cb7d78b805af5ed4cfab7d94897c67abd5
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-01-02 12:59:27 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (11.35%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -22,28 +22,55 @@ class Solution:
 
         # return ans
 
+        # # ans list
+        # ans = []
+
+        # for num in range(1,n+1):
+
+        #     divisible_by_3 = (num % 3 == 0)
+        #     divisible_by_5 = (num % 5 == 0)
+
+        #     num_ans_str = ""
+
+        #     if divisible_by_3:
+        #         # Divides by 3
+        #         num_ans_str += "Fizz"
+        #     if divisible_by_5:
+        #         # Divides by 5
+        #         num_ans_str += "Buzz"
+        #     if not num_ans_str:
+        #         # Not divisible by 3 or 5
+        #         num_ans_str = str(num)
+
+        #     # Append the current answer str to the ans list
+        #     ans.append(num_ans_str)  
+
+        # return ans
+        
+
         # ans list
         ans = []
 
-        for num in range(1,n+1):
+        # Dictionary to store all fizzbuzz mappings
+        fizz_buzz_dict = {3 : "Fizz", 5 : "Buzz"}
+        
+        # List of divisors which we will iterate over.
+        divisors = [3, 5]
 
-            divisible_by_3 = (num % 3 == 0)
-            divisible_by_5 = (num % 5 == 0)
+        for num in range(1, n + 1):
 
-            num_ans_str = ""
+            num_ans_str = []
 
-            if divisible_by_3:
-                # Divides by 3
-                num_ans_str += "Fizz"
-            if divisible_by_5:
-                # Divides by 5
-                num_ans_str += "Buzz"
+            for key in divisors:
+                # If the num is divisible by key,
+                # then add the corresponding string mapping to current num_ans_str
+                if num % key == 0:
+                    num_ans_str.append(fizz_buzz_dict[key])
+
             if not num_ans_str:
-                # Not divisible by 3 or 5
-                num_ans_str = str(num)
+                num_ans_str.append(str(num))
 
             # Append the current answer str to the ans list
-            ans.append(num_ans_str)  
+            ans.append(''.join(num_ans_str))
 
         return ans
-        
