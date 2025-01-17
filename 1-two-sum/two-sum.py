51274dcca635b573dd3a1c5968b73713de7cd859
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-01-17 18:49:16 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (19.00%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,18 +1,17 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        numMap = {}
-        n = len(nums)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # hashmap = {}
+        # for i in range(len(nums)):
+        #     complement = target - nums[i]
+        #     if complement in hashmap:
+        #         return [i, hashmap[complement]]
+        #     hashmap[nums[i]] = i
+        # # Return an empty list if no solution is found
+        # return []
+        numToIndex = {}
 
-        for i in range(n):
-            complement = target - nums[i]
-            if complement in numMap:
-                return [numMap[complement], i]
-            numMap[nums[i]] = i
-
-        return []
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+        for i, num in enumerate(nums):
+            if target - num in numToIndex:
+                return numToIndex[target - num], i
+            numToIndex[num] = i
         
