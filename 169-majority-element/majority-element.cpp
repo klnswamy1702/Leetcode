a4db7f0a40f9912400669f0a5f8dfbe951040dbb
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-01-23 08:48:21 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (23.85%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,9 +1,39 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        return nums[n/2];
+        // sort(nums.begin(), nums.end());
+        // int n = nums.size();
+        // return nums[n/2];
+
+        // int n = nums.size();
+        // unordered_map<int, int> m;
+        
+        // for(int i = 0; i < n; i++){
+        //     m[nums[i]]++;
+        // }
+        // n = n/2;
+        // for(auto x: m){
+        //     if(x.second > n){
+        //         return x.first;
+        //     }
+        // }
+        // return 0;
+        int count = 0;
+        int candidate = 0;
+        
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        
+        return candidate;
         
     }
 };
