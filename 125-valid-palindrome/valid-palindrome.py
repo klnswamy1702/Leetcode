52dab13a6ed372956148421579a28683ac7c17bc
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-04-10 05:21:39 +0530

    Time: 7 ms (80.70%) | Memory: 17.9 MB (95.34%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,17 +1,39 @@
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         # Create a list to hold only alphanumeric characters in lowercase form
+#         filtered_chars = [char.lower() for char in s if char.isalnum()]
+        
+#         # Initialize two pointers
+#         left, right = 0, len(filtered_chars) - 1
+        
+#         # Use two pointers to check palindrome
+#         while left < right:
+#             # Compare characters and move pointers
+#             if filtered_chars[left] != filtered_chars[right]:
+#                 return False
+#             left, right = left + 1, right - 1
+        
+#         return True
+        
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # Create a list to hold only alphanumeric characters in lowercase form
-        filtered_chars = [char.lower() for char in s if char.isalnum()]
-        
         # Initialize two pointers
-        left, right = 0, len(filtered_chars) - 1
+        left, right = 0, len(s) - 1
         
-        # Use two pointers to check palindrome
         while left < right:
-            # Compare characters and move pointers
-            if filtered_chars[left] != filtered_chars[right]:
+            # Move left pointer if not alphanumeric
+            while left < right and not s[left].isalnum():
+                left += 1
+            # Move right pointer if not alphanumeric
+            while left < right and not s[right].isalnum():
+                right -= 1
+            
+            # Compare characters in a case-insensitive manner
+            if s[left].lower() != s[right].lower():
                 return False
-            left, right = left + 1, right - 1
-        
+            
+            # Move both pointers towards the center
+            left += 1
+            right -= 1
+            
         return True
-        
