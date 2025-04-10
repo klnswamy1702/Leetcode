a2f2fe38d8ccc6dc637af57625ab011c1942e26f
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-04-10 06:14:39 +0530

    Time: 39 ms (60.83%) | Memory: 18 MB (15.10%) - LeetSync

--- 190-reverse-bits/reverse-bits.py
+++ 190-reverse-bits/reverse-bits.py
@@ -1,8 +1,23 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        res = 0
-        for i in range(32):
-            bit = (n >> i) & 1
-            res += (bit << (31 - i))
-        return res
+        # result = 0
+    
+        # for i in range(32):
+        #     # Extract the least significant bit of n
+        #     bit = n & 1
+            
+        #     # Shift the extracted bit to its correct reversed position
+        #     result = (result << 1) | bit
+            
+        #     # Prepare n for the next iteration by shifting right
+        #     n >>= 1
         
+        # return result
+        
+        result = 0
+        for _ in range(32):
+            # Combine shift and masking operations
+            result = (result << 1) | (n & 1)
+            n >>= 1  # Move to process next bit
+            
+        return result
