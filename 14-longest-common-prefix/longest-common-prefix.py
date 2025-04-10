30e1a8402bacce04fe31024c8f36251b46c75599
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-04-10 05:36:32 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (30.50%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,20 +1,21 @@
-# class Solution:
-#     def longestCommonPrefix(self, strs: List[str]) -> str:
-#         if not strs:
-#             return ""
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
     
-#         # Start with the full first string as a prefix
-#         prefix = strs[0]
+        # Start with the full first string as a prefix
+        prefix = strs[0]
         
-#         # Compare the prefix with each string one by one
-#         for s in strs[1:]:
-#             # Reduce the prefix size until it matches the beginning of `s`
-#             while not s.startswith(prefix):
-#                 prefix = prefix[:-1]  # Remove the last character from the prefix
-#                 if not prefix:  # If prefix becomes empty, no common prefix
-#                     return ""
+        # Compare the prefix with each string one by one
+        for s in strs[1:]:
+            # Reduce the prefix size until it matches the beginning of `s`
+            while not s.startswith(prefix):
+                prefix = prefix[:-1]  # Remove the last character from the prefix
+                if not prefix:  # If prefix becomes empty, no common prefix
+                    return ""
         
-#         return prefix
+        return prefix
+
 
 # class Solution:
 #     def longestCommonPrefix(self, strs: List[str]) -> str:
@@ -31,22 +32,6 @@
 #                     return strs[0][:i]
         
 #         return strs[0]
-
-class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        if not strs:
-            return ""
-    
-        # Check each column in each row
-        for i in range(len(strs[0])):
-            char = strs[0][i]
-            
-            for s in strs[1:]:
-                # If the current string length is less than i or characters mismatch
-                if i >= len(s) or s[i] != char:
-                    return strs[0][:i]
-        
-        return strs[0]
 
 # class Solution:
 #     def longestCommonPrefix(self, strs: List[str]) -> str:
