1e217a4a899ba3785ebd82e0cd70046de0506a6e
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-04-10 05:34:43 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (30.50%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -16,22 +16,85 @@
         
 #         return prefix
 
+# class Solution:
+#     def longestCommonPrefix(self, strs: List[str]) -> str:
+#         if not strs:
+#             return ""
+    
+#         # Check each column in each row
+#         for i in range(len(strs[0])):
+#             char = strs[0][i]
+            
+#             for s in strs[1:]:
+#                 # If the current string length is less than i or characters mismatch
+#                 if i >= len(s) or s[i] != char:
+#                     return strs[0][:i]
+        
+#         return strs[0]
+
+# class Solution:
+#     def longestCommonPrefix(self, strs: List[str]) -> str:
+#         if not strs:
+#             return ""
+    
+#         # Check each column in each row
+#         for i in range(len(strs[0])):
+#             char = strs[0][i]
+            
+#             for s in strs[1:]:
+#                 # If the current string length is less than i or characters mismatch
+#                 if i >= len(s) or s[i] != char:
+#                     return strs[0][:i]
+        
+#         return strs[0]
+
+# class Solution:
+#     def longestCommonPrefix(self, strs: List[str]) -> str:
+#         if not strs:
+#             return ""
+    
+#         def common_prefix(left, right):
+#             min_len = min(len(left), len(right))
+#             for i in range(min_len):
+#                 if left[i] != right[i]:
+#                     return left[:i]
+#             return left[:min_len]
+        
+#         def longest_common_prefix_recursive(l, r):
+#             if l == r:
+#                 return strs[l]
+#             else:
+#                 # Divide the list into two halves
+#                 mid = (l + r) // 2
+#                 lcp_left = longest_common_prefix_recursive(l, mid)
+#                 lcp_right = longest_common_prefix_recursive(mid + 1, r)
+#                 # Conquer/Combine step
+#                 return common_prefix(lcp_left, lcp_right)
+        
+#         return longest_common_prefix_recursive(0, len(strs) - 1)
+
+
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if not strs:
             return ""
-    
-        # Check each column in each row
-        for i in range(len(strs[0])):
-            char = strs[0][i]
-            
-            for s in strs[1:]:
-                # If the current string length is less than i or characters mismatch
-                if i >= len(s) or s[i] != char:
-                    return strs[0][:i]
+        # Find the minimum length string in strs
+        min_length = min(len(s) for s in strs)
         
-        return strs[0]
-
+        low, high = 0, min_length
+        
+        def is_common_prefix(length):
+            prefix = strs[0][:length]
+            return all(s.startswith(prefix) for s in strs[1:])
+        
+        while low <= high:
+            mid = (low + high) // 2
+            if is_common_prefix(mid):
+                low = mid + 1
+            else:
+                high = mid - 1
+        
+        return strs[0][:(low + high) // 2]
 
 
         
