90725c700670931f090c39ade794ab2dfc13005d
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-07 13:43:58 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.cpp
@@ -0,0 +1,14 @@
+// Last updated: 07/11/2025, 13:43:57
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+         int mid, left = 0, right = nums.size() - 1;
+    while (left <= right) {
+      mid = left + (right - left) / 2;
+      if (nums[mid] == target) return mid;
+      if (target < nums[mid]) right = mid - 1;
+      else left = mid + 1;
+    }
+    return -1;
+    }
+};
