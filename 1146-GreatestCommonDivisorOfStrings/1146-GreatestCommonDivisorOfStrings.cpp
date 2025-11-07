791cc62ba6b72887b0d2fcfc0a158b9e8063360e
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-07 13:43:07 +0530

    Create 1146-GreatestCommonDivisorOfStrings

---
+++ 1146-GreatestCommonDivisorOfStrings/1146-GreatestCommonDivisorOfStrings.cpp
@@ -0,0 +1,9 @@
+// Last updated: 07/11/2025, 13:43:07
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        return (str1 + str2 == str2 + str1)? 
+        str1.substr(0, gcd(size(str1),size(str2))): "";
+        
+    }
+};
