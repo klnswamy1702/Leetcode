64f586ba8f3fce8bcee9f8ea18f270ea7b1327d9
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-07 13:44:27 +0530

    Create 577-EmployeeBonus

---
+++ 577-EmployeeBonus/577-EmployeeBonus.sql
@@ -0,0 +1,16 @@
+-- Last updated: 07/11/2025, 13:44:27
+# Write your MySQL query statement below
+
+# SELECT name, bonus
+# FROM Employee LEFT JOIN Bonus USING(empId)
+# WHERE COALESCE(bonus, 0) < 1000; 
+
+# select E.name, B.bonus
+# from Employee as E LEFT JOIN Bonus as B
+# on E.empId= B.empId
+# where B.bonus < 1000 or B.bonus is NULL;
+
+select E.name, B.bonus
+from Employee as E LEFT JOIN Bonus as B
+on E.empId= B.empId
+where COALESCE(bonus, 0) < 1000;
