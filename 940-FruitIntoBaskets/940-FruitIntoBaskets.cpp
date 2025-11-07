f830fe6441279160cb21aa0ada58fa534e67d6e4
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-07 13:43:27 +0530

    Create 940-FruitIntoBaskets

---
+++ 940-FruitIntoBaskets/940-FruitIntoBaskets.cpp
@@ -0,0 +1,17 @@
+// Last updated: 07/11/2025, 13:43:27
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        unordered_map<int, int> count;
+        int i, j;
+        for (i = 0, j = 0; j < fruits.size(); ++j) {
+            count[fruits[j]]++;
+            if (count.size() > 2) {
+                if (--count[fruits[i]] == 0)count.erase(fruits[i]);
+                i++;
+            }
+        }
+        return j - i;
+        
+    }
+};
