0e640126d484e19c4ec539ed3eecf90de8b6ee40
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-07 13:43:26 +0530

    Create 966-BinarySubarraysWithSum

---
+++ 966-BinarySubarraysWithSum/966-BinarySubarraysWithSum.py
@@ -0,0 +1,10 @@
+# Last updated: 07/11/2025, 13:43:26
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        c = collections.Counter({0: 1})
+        psum = res = 0
+        for i in nums:
+            psum += i
+            res += c[psum - goal]
+            c[psum] += 1
+        return res
