9f80a9a68f930243f9ab052ef291a2d07aee6e0f
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-07 13:41:11 +0530

    Create 2085-ArrayWithElementsNotEqualToAverageOfNeighbors

---
+++ 2085-ArrayWithElementsNotEqualToAverageOfNeighbors/2085-ArrayWithElementsNotEqualToAverageOfNeighbors.py
@@ -0,0 +1,21 @@
+# Last updated: 07/11/2025, 13:41:11
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        nums.sort()
+
+        i, j, n = 0, 0, len(nums)
+        ans = [0]*n
+
+        while i < n and j < n:
+            ans[i] = nums[j]
+            i = i + 2
+            j = j + 1
+
+        i = 1
+        while i < n and j < n:
+            ans[i] = nums[j]
+            i = i + 2
+            j = j + 1
+
+        return ans
+        
