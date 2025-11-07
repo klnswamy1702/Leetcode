796408e50b2e9637bf233c03bbb5ad968d687489
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-07 13:40:38 +0530

    Create 2628-MinimizeTheMaximumOfTwoArrays

---
+++ 2628-MinimizeTheMaximumOfTwoArrays/2628-MinimizeTheMaximumOfTwoArrays.py
@@ -0,0 +1,20 @@
+# Last updated: 07/11/2025, 13:40:37
+# class Solution:
+#     def minimizeSet(self, divisor1: int, divisor2: int, uniqueCnt1: int, uniqueCnt2: int) -> int:
+class Solution:
+    def minimizeSet(self, D1: int, D2: int, C1: int, C2: int) -> int:
+        L, R, G = 0, 10**10, lcm(D1, D2)
+
+        while L < R:
+            
+            M = (L+R)//2                # [0] try middle value
+            
+            x = M - M//D1 >= C1         # [1] criterion 1
+            y = M - M//D2 >= C2         # [2] criterion 2
+            z = M - M//G  >= C1 + C2    # [3] criterion 3
+            
+            if x and y and z : R = M    # [4] classical step of
+            else             : L = M+1  #     the binary search
+
+        return L
+        
