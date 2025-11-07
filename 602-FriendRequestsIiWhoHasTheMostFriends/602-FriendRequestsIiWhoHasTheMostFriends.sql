8617aed74ea186a9ebd72292326e44548d54cfe2
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-07 13:44:19 +0530

    Create 602-FriendRequestsIiWhoHasTheMostFriends

---
+++ 602-FriendRequestsIiWhoHasTheMostFriends/602-FriendRequestsIiWhoHasTheMostFriends.sql
@@ -0,0 +1,12 @@
+-- Last updated: 07/11/2025, 13:44:19
+# Write your MySQL query statement below
+
+select id1 as id, count(id2) as num
+from
+(select requester_id as id1, accepter_id as id2 
+from RequestAccepted
+union
+select accepter_id as id1, requester_id as id2 
+from RequestAccepted) tmp1
+group by id1 
+order by num desc limit 1
