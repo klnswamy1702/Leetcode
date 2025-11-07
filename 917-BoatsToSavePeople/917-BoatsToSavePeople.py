909fc0b0845de721f4cf894d60171995ffdadfe4
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-07 13:43:33 +0530

    Create 917-BoatsToSavePeople

---
+++ 917-BoatsToSavePeople/917-BoatsToSavePeople.py
@@ -0,0 +1,13 @@
+# Last updated: 07/11/2025, 13:43:33
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        right = len(people) - 1
+        left = res = 0
+        while left <= right:
+            if people[left] + people[right] <= limit:
+                left += 1
+            right -= 1
+            res += 1
+        return res
+        
