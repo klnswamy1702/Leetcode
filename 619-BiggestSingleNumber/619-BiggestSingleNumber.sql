d8ea74031d837bee80ca8312fa4e8cea4c22e608
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-07 13:44:15 +0530

    Create 619-BiggestSingleNumber

---
+++ 619-BiggestSingleNumber/619-BiggestSingleNumber.sql
@@ -0,0 +1,22 @@
+-- Last updated: 07/11/2025, 13:44:14
+# Write your MySQL query statement below
+
+
+select(
+  select num
+  from MyNumbers
+  group by num
+  having count(*) = 1
+  order by num desc limit 1
+) as num;
+
+# SELECT num
+# FROM MyNumbers
+# GROUP BY num
+# HAVING count(*) = 1
+
+# UNION ALL
+# SELECT NULL
+
+# ORDER BY num DESC
+# LIMIT 1;
