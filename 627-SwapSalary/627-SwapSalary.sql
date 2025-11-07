d3998dc10ba19ccd482d37e7cacc8dec00a681ae
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-07 13:44:12 +0530

    Create 627-SwapSalary

---
+++ 627-SwapSalary/627-SwapSalary.sql
@@ -0,0 +1,11 @@
+-- Last updated: 07/11/2025, 13:44:12
+# Write your MySQL query statement below
+
+UPDATE salary SET sex =
+CASE sex
+    WHEN 'm' THEN 'f'
+    ELSE 'm'
+END;
+
+# update salary
+# set sex=if(sex='m','f','m');
