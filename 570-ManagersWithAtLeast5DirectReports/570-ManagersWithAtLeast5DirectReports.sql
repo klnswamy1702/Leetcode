5066ccfb16adb25de0ec904d7158e2dd0062296c
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-07 13:44:29 +0530

    Create 570-ManagersWithAtLeast5DirectReports

---
+++ 570-ManagersWithAtLeast5DirectReports/570-ManagersWithAtLeast5DirectReports.sql
@@ -0,0 +1,23 @@
+-- Last updated: 07/11/2025, 13:44:29
+# Write your MySQL query statement below
+
+select name from employee 
+where id in 
+(select managerId from Employee
+group by managerId
+having count(managerId)>=5) 
+
+# Write your MySQL query statement below
+# Select m.name
+# from employee as e
+# inner join employee as m
+# on e.managerId=m.id
+# group by e.managerId 
+# having count(e.id)>=5
+
+
+# select distinct m.name as name
+# from employee e join employee m
+# on m.id = e.managerid
+# group by m.id
+# having count(*) >= 5
