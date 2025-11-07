a378eeb70de98cbbf8427c1b6f9109ddc3b8fce9
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-07 13:44:30 +0530

    Create 553-OptimalDivision

---
+++ 553-OptimalDivision/553-OptimalDivision.py
@@ -0,0 +1,8 @@
+# Last updated: 07/11/2025, 13:44:30
+class Solution:
+    def optimalDivision(self, nums: List[int]) -> str:
+        if len(nums) == 1:
+            return str(nums[0])
+        if len(nums) == 2:
+            return str(nums[0]) + '/' + str(nums[1])
+        return str(nums[0]) + '/(' + '/'.join(map(str, nums[1:])) + ')'
