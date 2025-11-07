db0997f81f0e02fa1864f92c9c88bf911f771fd8
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-07 13:43:34 +0530

    Create 966-BinarySubarraysWithSum

---
+++ 966-BinarySubarraysWithSum/966-BinarySubarraysWithSum.cpp
@@ -0,0 +1,15 @@
+// Last updated: 07/11/2025, 13:43:34
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        unordered_map<int, int> c({{0, 1}});
+        int psum = 0, res = 0;
+        for (int i : nums) {
+            psum += i;
+            res += c[psum - goal];
+            c[psum]++;
+        }
+        return res;
+        
+    }
+};
