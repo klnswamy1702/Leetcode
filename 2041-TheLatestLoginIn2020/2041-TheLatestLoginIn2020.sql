0eefe23ff28bc8023ee2639f9730f850d185f8fe
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-07 13:41:13 +0530

    Create 2041-TheLatestLoginIn2020

---
+++ 2041-TheLatestLoginIn2020/2041-TheLatestLoginIn2020.sql
@@ -0,0 +1,9 @@
+-- Last updated: 07/11/2025, 13:41:12
+# Write your MySQL query statement below
+SELECT
+    user_id,
+    MAX(time_stamp) AS last_stamp 
+    
+FROM Logins
+WHERE YEAR(time_stamp) = 2020 
+GROUP BY user_id;
