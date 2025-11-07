e281e697c791e9a183eb339663f39635fb57688e
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-07 13:44:28 +0530

    Create 584-FindCustomerReferee

---
+++ 584-FindCustomerReferee/584-FindCustomerReferee.sql
@@ -0,0 +1,8 @@
+-- Last updated: 07/11/2025, 13:44:28
+# Write your MySQL query statement below
+# select name from Customer
+# where referee_id!=2 or referee_id is null;
+
+SELECT name
+FROM Customer
+WHERE COALESCE(referee_id,0) <> 2;
