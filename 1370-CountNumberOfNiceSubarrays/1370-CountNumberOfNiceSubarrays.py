bf0410ac636197a705120cd2a32d8cac1840e039
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-07 13:42:47 +0530

    Create 1370-CountNumberOfNiceSubarrays

---
+++ 1370-CountNumberOfNiceSubarrays/1370-CountNumberOfNiceSubarrays.py
@@ -0,0 +1,14 @@
+# Last updated: 07/11/2025, 13:42:47
+class Solution:
+    def numberOfSubarrays(self, A: List[int], k: int) -> int:
+        def atMost(k):
+            res = i = 0
+            for j in range(len(A)):
+                k -= A[j] % 2
+                while k < 0:
+                    k += A[i] % 2
+                    i += 1
+                res += j - i + 1
+            return res
+
+        return atMost(k) - atMost(k - 1)
