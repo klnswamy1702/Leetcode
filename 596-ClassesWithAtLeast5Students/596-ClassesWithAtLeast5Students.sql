43d7551a32b097577866e195bf52120de6bcf0b1
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-07 13:44:24 +0530

    Create 596-ClassesWithAtLeast5Students

---
+++ 596-ClassesWithAtLeast5Students/596-ClassesWithAtLeast5Students.sql
@@ -0,0 +1,11 @@
+-- Last updated: 07/11/2025, 13:44:23
+
+select class
+from 
+    (select class, count(distinct student) as num
+    from courses
+    group by class) as temp
+where num >= 5;
+
+
+
