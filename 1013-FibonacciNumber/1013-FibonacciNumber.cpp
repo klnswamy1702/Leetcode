4eb4a34a78a080de3245fd2b20567699b8180afd
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-07 13:43:21 +0530

    Create 1013-FibonacciNumber

---
+++ 1013-FibonacciNumber/1013-FibonacciNumber.cpp
@@ -0,0 +1,8 @@
+// Last updated: 07/11/2025, 13:43:21
+class Solution {
+public:
+    int fib(int n) {
+        double phi = (sqrt(5) + 1) / 2;     
+        return round(pow(phi, n) / sqrt(5));
+    }
+};
