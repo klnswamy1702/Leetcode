a602ad6de216e83a84caec101359e317eb6f7f37
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-07 13:41:20 +0530

    Create 1942-PrimaryDepartmentForEachEmployee

---
+++ 1942-PrimaryDepartmentForEachEmployee/1942-PrimaryDepartmentForEachEmployee.sql
@@ -0,0 +1,15 @@
+-- Last updated: 07/11/2025, 13:41:20
+# Write your MySQL query statement below
+# select employee_id,
+#        coalesce(max(case when primary_flag='Y' then department_id else null end),max(department_id)) department_id
+#   from employee t
+#   group by employee_id
+
+SELECT employee_id, department_id 
+FROM Employee
+WHERE primary_flag = 'Y'
+UNION
+SELECT employee_id, department_id 
+FROM Employee 
+GROUP BY employee_id
+HAVING COUNT(employee_id) = 1
